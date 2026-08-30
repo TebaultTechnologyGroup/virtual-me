481b4a1e3bd498d5648948106b0dcc02cdf175d0
TebaultTechnologyGroup <mark.tebault@tebaulttechnologygroup.com>
2026-08-29 21:56:50 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import "./index.css";
+
+export default function App() {
+  return <div className="bg-primary text-primary-foreground">Hello</div>;
+}
---
+++ src/lib/supabase_client.ts
@@ -0,0 +1,17 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabasePubKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+
+// export const test = () => {
+//   console.log("URL:", supabaseUrl);
+//   console.log("Key:", supabasePubKey);
+// };
+
+export const supabase = createClient(supabaseUrl, supabasePubKey, {
+    auth: {
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: true,
+    },
+});
